b38d6ea626e466f294269ccb6997e5937e2324b9
Samuel Zenebe <samuelzenebe7@gmail.com>
2026-03-05 00:25:23 -0800

    fix: build error and vitest testing errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-// vite.config.ts
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
   test: {
-    globals: true,
     environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    globals: true,
   },
 });
